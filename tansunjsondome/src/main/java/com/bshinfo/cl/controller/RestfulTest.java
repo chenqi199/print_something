100a6e57aa92682d6f2efc14648cd76d91db22b9
陈奇 <892854990@qq.com>
2017-07-05 06:47:13 +0800

    页面

--- tansunjsondome/src/main/java/com/bshinfo/cl/controller/RestfulTest.java
+++ tansunjsondome/src/main/java/com/bshinfo/cl/controller/RestfulTest.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by 陈奇 on 2017/5/3 0003.
@@ -19,6 +22,17 @@ public class RestfulTest {
     @ResponseBody
     public String index(@PathVariable("id") String id) throws UnsupportedEncodingException {
        return new    String(id.getBytes("iso8859-1"),"utf-8");
+    }
+
+
+ @RequestMapping(value = "/htmlDemo", method = RequestMethod.POST, produces = "text/html;charset=UTF-8")
+    public ModelAndView index() {
+     Map<String,String> data = new HashMap();
+     data.put("name","chenqi");
+     data.put("aga","27");
+     data.put("aa","bb");
+     data.put("cc","dd");
+     return new ModelAndView("/demo" ,data);
     }
 
 
---
+++ tansunjsondome/src/main/webapp/views/demo.ftl
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="utf-8"/>
+    <title>人寿角色管理</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
+
+</head>
+<!-- END HEAD -->
+
+<body>
+<!-- BEGIN PAGE CONTENT-->
+<div >
+<table>
+    <thead>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+        <td>16</td>
+        <td>${name}</td>
+    </tr>
+    </tbody>
+</table>
+
+</div>
+
+</body>
+
+</html>
